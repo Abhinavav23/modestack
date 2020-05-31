d849fdb943e8a2c124129de559da745206c5add5
Abhinavab <abhinavav23@gmail.com>
2020-05-31 18:48:09 +0530

    changes for angular version and removed .

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + './dist/modestack-login'));
+app.use(express.static(__dirname + '/dist/modestack-login'));
 
 app.get('/*', function(req,res) {
     
