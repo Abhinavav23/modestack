192a30fb712dab5134bedbc59faf918e3e9192e2
Abhinavab <abhinavav23@gmail.com>
2020-05-31 05:53:16 +0530

    changed server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/{{modestack-login}}'));
+app.use(express.static('./dist/modestack-login'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{modestack-login}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/modestack-login/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
